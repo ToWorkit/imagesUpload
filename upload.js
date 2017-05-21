bc3372b567aa7850a50a3ef0f01966c9c29b0d37
Toworkit <1211126374@qq.com>
2017-05-21 09:47:37 +0800

    文件base64形式上传并显示

--- upload.js
+++ upload.js
@@ -1,7 +1,21 @@
-(function (win, doc) {
   // 选择器
   function $(element) {
-    return doc.querySelector(element);
+    return document.querySelector(element);
   }
-  console.log($('#fileUpload'))
-})(window, document)
+  // 测试
+  function fileUpload(files){
+    let imgStr = '';
+    // console.log(files)
+    let file = files[0]
+    // 创建实例
+    let render = new FileReader();
+    // 使用FileReader 的 readAsDataURL方法
+    // 会将图片进行base 64转码
+    render.readAsDataURL(file);
+    // 成功后
+    render.onload = function (e) {
+      imgStr += '<span><img src="'+ e.target.result +'" alt="" /></span>'
+      $("#prevUpload").innerHTML = imgStr
+    }
+  }
+  // $('#fileUpload').addEventLister("change", fileUpload(this.files))
